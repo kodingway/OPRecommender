2ec78acaed99d36f69a361b1e380f6535dd2839c
stsirtsis <stsirtsis@gmail.com>
2018-12-24 13:56:41 +0200

    Minor modifications

--- heuristics.py
+++ heuristics.py
@@ -3,7 +3,6 @@ import numpy as np
 import random
 import math
 import matplotlib.pyplot as plt
-from joblib import Parallel, delayed
 
 city = "Rome"
 graph = readGraph(city)
@@ -11,52 +10,11 @@ users = readUsers(city)
 pois = readPOIs(city,len(graph))
 stayTime = readStayTimes(city,len(graph))
 
-def heuristicsRep(rep):
-    
-    with open('log.txt','a') as f:
-        f.write(str(rep)+'\n')
-    
-    while True:
-        (s, t) = random.sample(range(1,len(graph)), 2)
-        if pathCost([s,t],stayTime,graph) <= B:
-            break
-
-    #print(rep)
-    distanceScore = []
-    valueScore = []
-    ratioScore = []
-    ratioPScore = []
-    # ratioPPScore = []
-
-    distPath = bestDistancePath(s,t,graph,stayTime,B)
-    
-    for k in klist:
-        #print(rep,k)
-        testUsers = []
-        testSet = random.sample(range(1,len(users)+1), k)
-        for l in testSet:
-            testUsers.append(users[l])
-        testUsers = np.array([np.array(xi) for xi in testUsers])
-
-        distanceScore.append(pathProfit(distPath,testUsers,scoreFunc,pois))
-        
-        valPath = bestValuePath(s, t, testUsers,graph,scoreFunc,stayTime,B,pois)
-        ratPath = bestRatioPath(s, t, testUsers,graph,scoreFunc,stayTime,B,pois)
-        ratPPath = bestRatioPlusPath(s, t, testUsers,graph,scoreFunc,stayTime,B,pois)
-        # ratPPPath = bestRatioPlusPlusPath(s,t,testUsers,graph,scoreFunc,stayTime,B,pois)
-        
-        valueScore.append(pathProfit(valPath, testUsers, scoreFunc,pois))
-        ratioScore.append(pathProfit(ratPath, testUsers, scoreFunc,pois))
-        ratioPScore.append(pathProfit(ratPPath, testUsers, scoreFunc,pois))
-        # ratioPPScore.append(pathProfit(ratPPPath,testUsers,scoreFunc,pois))
-
-    return (distanceScore,valueScore,ratioScore,ratioPScore)
-
 ########## HEURISTICS SIMULATION & COMPARISON ##########
 ########################################################
 ########################################################
 scoreFunc = satisfactionSum
-totalReps = 500
+totalReps = 100
 B = 420  # Budget (minutes)
 klist=[1,2,5,10,20]
 
@@ -66,32 +24,72 @@ totalRatioScore=[]
 totalRatioPScore=[]
 # totalRatioPPScore=[]
 
-results  = Parallel(n_jobs=15)(delayed(heuristicsRep)(rep) for rep in range(totalReps))
-totalDistanceScore = [res[0] for res in results]
-totalValueScore = [res[1] for res in results]
-totalRatioScore = [res[2] for res in results]
-totalRatioPScore = [res[3] for res in results]
+for rep in range(totalReps):
+
+    while True:
+        (s, t) = random.sample(range(1,len(graph)), 2)
+        if pathCost([s,t],stayTime,graph) <= B:
+            break
+
+    print(rep)
+    distanceScore = []
+    valueScore = []
+    ratioScore = []
+    ratioPScore = []
+    # ratioPPScore = []
+
+    # print("Best distance")
+    distPath = bestDistancePath(s,t,graph,stayTime,B)
+    
+    for k in klist:
+        print(rep,k)
+        testUsers = []
+        testSet = random.sample(range(1,len(users)+1), k)
+        for l in testSet:
+            testUsers.append(users[l])
+        testUsers = np.array([np.array(xi) for xi in testUsers])
+
+        distanceScore.append(pathProfit(distPath,testUsers,scoreFunc,pois))
+        # print("Best value")
+        valPath = bestValuePath(s, t, testUsers,graph,scoreFunc,stayTime,B,pois)
+        # print("Best ratio")
+        ratPath = bestRatioPath(s, t, testUsers,graph,scoreFunc,stayTime,B,pois)
+        # print("Best ratio+")
+        # ratPPath = bestRatioPlusPath(s, t, testUsers,graph,scoreFunc,stayTime,B,pois)
+        # print("Best ratio++")
+        # ratPPPath = bestRatioPlusPlusPath(s,t,testUsers,graph,scoreFunc,stayTime,B,pois)
+        
+        valueScore.append(pathProfit(valPath, testUsers, scoreFunc,pois))
+        ratioScore.append(pathProfit(ratPath, testUsers, scoreFunc,pois))
+        # ratioPScore.append(pathProfit(ratPPath, testUsers, scoreFunc,pois))
+        # ratioPPScore.append(pathProfit(ratPPPath,testUsers,scoreFunc,pois))
+
+    totalDistanceScore.append(distanceScore)
+    totalValueScore.append(valueScore)
+    totalRatioScore.append(ratioScore)
+    # totalRatioPScore.append(ratioPScore)
+    # totalRatioPPScore.append(ratioPPScore)
 
 with open('heuristics.dat','w') as f:
     f.write(str(totalDistanceScore))
     f.write(str(totalValueScore))
     f.write(str(totalRatioScore))
-    f.write(str(totalRatioPScore))
-    # f.write(bestRatioPlusPlus)
+    # f.write(str(totalRatioPScore))
+    # f.write(str(totalRatioPPScore))
 
 bestDistance = np.mean(totalDistanceScore,axis=0)
 bestValue = np.mean(totalValueScore, axis=0)
 bestRatio = np.mean(totalRatioScore, axis=0)
-bestRatioPlus = np.mean(totalRatioPScore, axis=0)
+# bestRatioPlus = np.mean(totalRatioPScore, axis=0)
 # bestRatioPlusPlus = np.mean(totalRatioPPScore, axis=0)
 
 plt.figure()
 plt.plot(klist,bestDistance, marker='v', color='purple', linestyle='--')
 plt.plot(klist,bestValue, marker='^', color='aqua', linestyle='--')
 plt.plot(klist,bestRatio, marker='D', color='r', linestyle='--')
-plt.plot(klist,bestRatioPlus, marker='s', color='fuchsia', linestyle='--')
+# plt.plot(klist,bestRatioPlus, marker='s', color='fuchsia', linestyle='--')
 # plt.plot(klist,bestRatioPlusPlus, marker='o', color='darkblue', linestyle='--')
-plt.legend(['bestDistance', 'bestValue','bestRatio','bestRatio+'])
+plt.legend(['bestDistance', 'bestValue','bestRatio'])
 plt.xticks(klist)
 plt.xlabel('Group size')
 plt.ylabel('Average Solution Value')
