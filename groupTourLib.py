2a5aa85227ab378e179e24d731be2e836f898726
stsirtsis <stsirtsis@gmail.com>
2018-12-24 14:33:20 +0200

    Modified BR+ to maximize both ratio and score

--- groupTourLib.py
+++ groupTourLib.py
@@ -174,7 +174,7 @@ def bestRatioPlusPath(s, t, testUsers, graph, scoreFunc, stayTime, B,pois):
                 for nodeI in range(1,len(path)-1):
                     potPath = path.copy()
                     potPath[nodeI] = neigh
-                    if (pathProfit(potPath,testUsers,scoreFunc,pois)/pathCost(potPath,stayTime,graph)>pathProfit(bestPath,testUsers,scoreFunc,pois)/pathCost(bestPath,stayTime,graph)) and (pathCost(potPath,stayTime,graph) <= B):
+                    if (pathProfit(potPath,testUsers,scoreFunc,pois)/pathCost(potPath,stayTime,graph)>pathProfit(bestPath,testUsers,scoreFunc,pois)/pathCost(bestPath,stayTime,graph)) and (pathProfit(potPath,testUsers,scoreFunc,pois)>pathProfit(bestPath,testUsers,scoreFunc,pois)) and (pathCost(potPath,stayTime,graph) <= B):
                         bestPath = potPath.copy()
         if bestPath == path:
             return path
